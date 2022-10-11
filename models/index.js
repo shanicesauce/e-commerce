5e4f500e99ea14ac4272fe51ab72956b89fcaab2
shanicesauce <shanicesauceda@gmail.com>
2022-10-11 00:53:08 -0500

    created models

---
+++ models/index.js
@@ -0,0 +1,42 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tags, {
+  through: ProductTag,
+  as:'products',
+  foreignKey:'product_id'
+});
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as:'tags',
+  foreignKey:'tag_id'
+});
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+}),
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
